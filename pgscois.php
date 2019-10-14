f3f9fb514118ddceacf4c5d9eb7e296299ea89ba
AustinCabs <austinbrein@outlook.com>
2019-10-14 17:01:46 +0800

    first commit

---
+++ pgscois.php
@@ -0,0 +1,149 @@
+<html>
+
+<head>
+<link rel="stylesheet" href="assets/bootstrap-4.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="function/css/mycss.css">
+<link rel="stylesheet" href="function/css/animate.css">
+<link href="assets/open-iconic-master/font/css/open-iconic-bootstrap.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Montserrat:700|Poppins&display=swap" rel="stylesheet">
+
+    <title></title>
+</head>
+
+<body>
+    
+    <div class="col-lg-12 text-center mt-5">
+
+        <img src="iPOD/img/socot_logo.png">
+
+        <h1 class="outline"style="font-family: 'Montserrat', sans-serif;
+        -webkit-text-stroke: 1px black;
+        color: white;
+        text-shadow:
+        3px 3px 0 #000,
+        -1px -1px 0 #000,  
+        1px -1px 0 #000,
+        -1px  1px 0 #000,
+        1px  1px 0 #000; ">
+        PROVINCIAL GOVERMENT OF SOUTH COTABATO <br> <h4 style="font-family: 'Poppins',sans-serif; 
+        color: white;
+        text-shadow:
+        3px 3px 0 #000,
+        -1px -1px 0 #000,  
+        1px -1px 0 #000,
+        -1px  1px 0 #000,
+        1px  1px 0 #000;">
+        Open Information System</h4> </h1>
+
+    </div>
+    <div class="col-md-4 offset-md-4 mt-5">
+            <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="" aria-label="Recipient's username" aria-describedby="basic-addon2">
+            <div class="input-group-append">
+                <button class="btn btn-info" type="submit"><span class="oi oi-magnifying-glass"></span> Search</button>
+            </div>
+            </div>
+    </div>
+
+    <div class="col-lg-12 text-center mt-5">
+         <div class="grid">
+              <div class="grid-sizer"></div>
+                <div class="grid-item"><div class="card" style="width: 15rem;">
+                          <img src="PGSC/images/sc.jpg" class="card-img-top" alt="...">
+                          <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="grid-item"><div class="card" style="width: 15rem;">
+                          <img src="PGSC/images/sc.jpg" class="card-img-top" alt="...">
+                          <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+
+                  <div class="grid-item"><div class="card" style="width: 15rem;">
+                          <img src="PGSC/images/sc.jpg" class="card-img-top" alt="...">
+                          <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+
+                         <div class="grid-item"><div class="card" style="width: 15rem;">
+                          <img src="PGSC/images/sc.jpg" class="card-img-top" alt="...">
+                          <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+
+                         <div class="grid-item"><div class="card" style="width: 15rem;">
+                          <img src="PGSC/images/sc.jpg" class="card-img-top" alt="...">
+                          <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="grid-item"><div class="card" style="width: 15rem;">
+                          <img src="PGSC/images/sc.jpg" class="card-img-top" alt="...">
+                          <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+
+                  <div class="grid-item"><div class="card" style="width: 15rem;">
+                          <img src="PGSC/images/sc.jpg" class="card-img-top" alt="...">
+                          <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+
+                         <div class="grid-item"><div class="card" style="width: 15rem;">
+                          <img src="PGSC/images/sc.jpg" class="card-img-top" alt="...">
+                          <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div> <!-- grid -->
+      
+    </div> <!-- col-lg-12 -->
+    
+</body>
+<script src="assets/jquery/jquery-3.4.1.js"></script>
+<script src="assets/wow.min.js"></script>
+<Script>  new WOW().init(); </Script>
+<script src="assets/bootstrap-4.3.1/js/bootstrap.min.js"></script>
+<script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
+<script>
+    $('.grid').masonry({
+      // options
+      itemSelector: '.grid-item',
+      columnWidth: '.grid-sizer',
+      fitWidth: true
+    });
+</script>
+</html>     
