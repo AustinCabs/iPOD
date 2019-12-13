9713ab2e207f226f221d8a62118ec729d4a2891c
AustinCabs <austinbrein@outlook.com>
2019-12-13 18:08:37 +0800

    edit footer pgscois

--- pgscois.php
+++ pgscois.php
@@ -65,89 +65,54 @@
 
 
 <!-- Footer -->
-		<footer class="pt-5 pb-4" id="contact">
+		<footer class="pt-3 pb-2" id="contact">
 			<div class="container">
 				<div class="row">
 					<div class="col-lg-3 col-md-6 col-sm-6 mt-2 mb-4">
-						<h5 class="mb-4 font-weight-bold">ABOUT US</h5>
-						<p class="mb-4">Etiam laoreet in ex quis efficitur.</p>
+						<h5 class="mb-4 font-weight-bold">YOU MAY VISIT US</h5>
 						<ul class="f-address">
 							<li>
 								<div class="row">
 									<div class="col-1"><i class="fas fa-map-marker"></i></div>
 									<div class="col-10">
-										<h6 class="font-weight-bold mb-0">Address:</h6>
-										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+										<h6 class="font-weight-bold mb-0"> </h6>
+										<p>Provincial Planning and Development Office - Special Projects Division</p>
 									</div>
 								</div>
 							</li>
 							<li>
 								<div class="row">
-									<div class="col-1"><i class="far fa-envelope"></i></div>
+									<div class="col-1"><i class="fas fa-map-marker"></i></div>
 									<div class="col-10">
-										<h6 class="font-weight-bold mb-0">Have any questions?</h6>
-										<p><a href="#">Support@userthemes.com</a></p>
-									</div>
-								</div>
-							</li>
-							<li>
-								<div class="row">
-									<div class="col-1"><i class="fas fa-phone-volume"></i></div>
-									<div class="col-10">
-										<h6 class="font-weight-bold mb-0">Address:</h6>
-										<p><a href="#">+XX (0) XX XX-XXXX-XXXX</a></p>
+										<h6 class="font-weight-bold mb-0"> </h6>
+										<p>PPDO, Provincial Capitol Compound, Alunan Ave.,
+										Brgy. Zone 3, City of Koronadal, South Cotabato, 9506</p>
 									</div>
 								</div>
 							</li>
 						</ul>
 					</div>
 					<div class="col-lg-3 col-md-6 col-sm-6 mt-2 mb-4">
-						<h5 class="mb-4 font-weight-bold">CONTACT INFORMATION</h5>
+						<h5 class="mb-4 font-weight-bold">HOTLINE</h5>
 						<ul class="f-address">
 							<li>
 								<div class="row">
-									<div class="col-1"><i class="fab fa-twitter"></i></div>
+									<div class="col-1"><i class="fas fa-tty"></i></div>
 									<div class="col-10">
-										<p class="mb-0"><a href="#">@userthemesrel </a> HTML Version Out Now</p>
-										<label>10 Mins Ago</label>
+										<p class="mb-0">(083) 228-9325 </p>
 									</div>
 								</div>
 							</li>
-							<li>
-								<div class="row">
-									<div class="col-1"><i class="fab fa-twitter"></i></div>
-									<div class="col-10">
-										<p class="mb-0"><a href="#">@userthemesrel </a> HTML Version Out Now</p>
-										<label>10 Mins Ago</label>
-									</div>
-								</div>
-							</li>
-							<li>
-								<div class="row">
-									<div class="col-1"><i class="fab fa-twitter"></i></div>
-									<div class="col-10">
-										<p class="mb-0"><a href="#">@userthemesrel </a> HTML Version Out Now</p>
-										<label>10 Mins Ago</label>
-									</div>
-								</div>
-							</li>
+						
 						</ul>
 					</div>
 					<div class="col-lg-3 col-md-6 col-sm-6 mt-2 mb-4">
-						<h5 class="mb-4 font-weight-bold">LATEST UPDATES</h5>
-						<ul class="recent-post">
-							<li>
-								<label class="mr-3">28 <br><span>APR</span></label>
-								<span>Rendomised words which dont look eveable.</span>
-							</li>
-							<li>
-								<label class="mr-3">29 <br><span>APR</span></label>
-								<span>Rendomised words which dont look eveable.</span>
-							</li>
-							<li>
-								<label class="mr-3">30 <br><span>APR</span></label>
-								<span>Rendomised words which dont look eveable.</span>
-							</li>
+						<h5 class="mb-4 font-weight-bold">SOCIAL MEDIA</h5>
+						<ul class="social">
+							<li><a href="#" title="facebook"><i class="fab fa-facebook-square"></i></a></li>
+							<li><a href="#" title="twitter"><i class="fab fa-twitter"></i></a></li>
+							<li><a href="#" title="google-plus"><i class="fab fa-google-plus-g"></i></a></li>
+							<li><a href="#" title="instagram"><i class="fab fa-instagram"></i></a></li>
 						</ul>
 					</div>
 					<div class="col-lg-3 col-md-6 col-sm-6 mt-2 mb-4">
@@ -155,12 +120,6 @@
 						<div class="input-group">
 						  	<a href="login.php" class="btn btn-info">Click to Login</a>
 						</div>
-						<ul class="social-pet mt-4">
-							<li><a href="#" title="facebook"><i class="fab fa-facebook-f"></i></a></li>
-							<li><a href="#" title="twitter"><i class="fab fa-twitter"></i></a></li>
-							<li><a href="#" title="google-plus"><i class="fab fa-google-plus-g"></i></a></li>
-							<li><a href="#" title="instagram"><i class="fab fa-instagram"></i></a></li>
-						</ul>
 					</div>
 				</div>
 			</div>
